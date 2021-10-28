38cf3ddcd080562cfc030a81f8916f8462012be7
Sandesh Ghone <sandesh.fluxbyte@gmail.com>
2021-10-28 14:53:24 +0530

    adding some style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { windowComponent } from './window/window.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    windowComponent
+    windowComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
